b00540c3d794862c3c2ff75a41c3e7da670b904d
cpg-qe <swathi.g@servicenow.com>
2025-04-29 07:54:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ resource "azurerm_storage_account" "stor" {
   resource_group_name      = "${azurerm_resource_group.rg.name}"
   account_tier             = "${var.storage_account_tier}"
   account_replication_type = "${var.storage_replication_type}"
+  allow_nested_items_to_be_public = false  # Disables public access
 }
 
 resource "azurerm_storage_account" "stor1" {
@@ -21,6 +22,7 @@ resource "azurerm_storage_account" "stor1" {
   resource_group_name      = "${azurerm_resource_group.rg.name}"
   account_tier             = "${var.storage_account_tier}"
   account_replication_type = "${var.storage_replication_type}"
+  allow_nested_items_to_be_public = false  # Disables public access
 }
 
 resource "azurerm_storage_account" "stor2" {
