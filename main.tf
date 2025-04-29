3909699b4a9585ab7db2b201d7eec2ba8f9d0d5e
cpg-qe <swathi.g@servicenow.com>
2025-04-29 07:51:03 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "random_id" "server" {
+  byte_length = 8
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "tfeautorgdnd-${random_id.server.hex}"
+  location = "${var.region}"
+}
+
+resource "azurerm_storage_account" "stor" {
+  name                     = "stor${random_id.server.hex}"
+  location                 = "${var.region}"
+  resource_group_name      = "${azurerm_resource_group.rg.name}"
+  account_tier             = "${var.storage_account_tier}"
+  account_replication_type = "${var.storage_replication_type}"
+}
+
+resource "azurerm_storage_account" "stor1" {
+  name                     = "stor1${random_id.server.hex}"
+  location                 = "${var.region}"
+  resource_group_name      = "${azurerm_resource_group.rg.name}"
+  account_tier             = "${var.storage_account_tier}"
+  account_replication_type = "${var.storage_replication_type}"
+}
+
+resource "azurerm_storage_account" "stor2" {
+  name                     = "stor2${random_id.server.hex}"
+  location                 = "${var.region}"
+  resource_group_name      = "${azurerm_resource_group.rg.name}"
+  account_tier             = "${var.storage_account_tier}"
+  account_replication_type = "${var.storage_replication_type}"
+  allow_nested_items_to_be_public = false  # Disables public access
+}
